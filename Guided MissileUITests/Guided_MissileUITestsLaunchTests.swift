290c8045b7e743b6fcc5cf5f4859e7c4328fd691
Bill Hause <bill.hause@yahoo.com>
2022-12-08 16:34:49 -0700

    Initial Commit

---
+++ Guided MissileUITests/Guided_MissileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Guided_MissileUITestsLaunchTests.swift
+//  Guided MissileUITests
+//
+//  Created by William Hause on 12/8/22.
+//
+
+import XCTest
+
+final class Guided_MissileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
